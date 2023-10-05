9904ca050c628350c001d47a9aa71954b37fd993
Priyanka Bagal <priyanka.bagal@tcgre.com>
2023-10-05 18:20:35 +0530

    add files

---
+++ src/main/java/Basic.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class Basic {
+
+	public static AndroidDriver capabilities() throws MalformedURLException {
+		DesiredCapabilities cap = new DesiredCapabilities();
+		
+		File f=new File("src");
+		File fs=new File(f,"ApiDemos-debug.apk");
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "AndroidDemo2");
+	    cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+	    
+	    cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,3000);
+	    AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+        return driver;
+	}
+	
+}
---
+++ src/main/java/HomePage.java
@@ -0,0 +1,25 @@
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class HomePage extends DriverInvoke{
+
+	public static void main(String[] args) throws MalformedURLException {
+		
+		AndroidDriver driver=capabilities();
+		System.out.println("Application started");
+        driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"App\"]")).click();
+        driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Alert Dialogs\"]")).click();
+        driver.findElement(By.id("io.appium.android.apis:id/text_entry_button")).click();
+        driver.findElement(By.id("io.appium.android.apis:id/username_edit")).sendKeys("LivingS");
+        driver.findElement(By.id("io.appium.android.apis:id/password_edit")).sendKeys("LS123");
+        driver.findElement(By.id("android:id/button1")).click();
+        System.out.println("Entered Credentials");
+	}
+
+
+}
+
